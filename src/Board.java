43a4c90596a9ee54b51109288957e54b874308ff
eyirik13 <eyirik13@ku.edu.tr>
2015-10-13 14:58:32 +0000

    pending boolean?, prototypeBoard'da kullan?l?yor

--- src/Board.java
+++ src/Board.java
@@ -1,8 +1,9 @@
 import java.util.ArrayList;
 
 
-public class Board { //superclass
-	private ArrayList<Square> Squares; 
+public class Board { //inherits other boards
+	private ArrayList<Square> Squares = new ArrayList<Square>(); 
+	private ArrayList<Boolean> pending = new ArrayList<Boolean>(); //pending kartlarının aktif olup olmadığını tutuyor
 
 	public void addSquare(Square sq){
 		Squares.add(sq);
@@ -14,6 +15,13 @@ public class Board { //superclass
 
 	public int getSquareId(Square sq){
 		return Squares.indexOf(sq);
+	}
+	
+	public void addPending(){
+		pending.add(false);
+	}
+	public void setPending(int index, boolean boo){
+		pending.set(index, boo);
 	}
 
 }
