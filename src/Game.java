b4cfda5bb2cb53f12a533a055e6d0105f335e963
fatihozh <fatihozh@localhost>
2015-10-17 20:46:15 +0300

    ?n modfikasyon

--- src/Game.java
+++ src/Game.java
@@ -2,9 +2,11 @@
 public class Game {
 
 	public static int numOfPlayers;
-	public PrototypeBoard board;
+	public static PrototypeBoard board;
 	public static Player[] players;
 	public static int currentPlayerID;
+	public static Dice dice;
+	public static GuiHandler gui;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
@@ -13,7 +15,7 @@ public class Game {
 		//Game Initializers
 			//GUI will inform how many users there will be (unnecessary for prototype:4)
 		numOfPlayers=4;
-		
+		int startMoney=0;
 		
 			//Board Initializers
 		
@@ -21,40 +23,52 @@ public class Game {
 			
 			//Dice initializer
 			
-			Dice dice = new Dice(board);
+			dice = new Dice(board);
 			
 			//Player Initializers
 			players = new Player[numOfPlayers];
+			String[] names = new String[4];
+			names[0] = "Ali";
+			names[1] = "Veli";
+			names[2] = "Deli";
+			names[3] = "Fethi";
+			for (int i = 0; i < players.length; i++) {
+				players[i] = new Player(i, names[i], startMoney, board.getSquareById(0));
+			}
 			//bütün playerları idleriyle construct et. for loop
 			currentPlayerID=0; // save load için burası modifiye edilecek
 			//GUI INIT
-			GuiHandler.Init(players, board);
-			GuiHandler.displayPlayer(currentPlayer()); //input olarak Player type
+			gui.Init(players, board);
+			gui.displayPlayer(currentPlayer()); //input olarak Player type
 		
 	}
-	private static void userRoll() {
+	private void userRoll() {
 		// TODO Auto-generated method stub
 		//Main Loop
 		//GUI nereden updatelenecek???
 			if(!(currentPlayer().isBankrupt())){
 				
-				Square temp = Dice.rollDice(currentPlayer()); 
-				GuiHandler.diceRolled(dice);// Dice alacak!!! iki int
-				GuiHandler.playerMoved(temp) //int indsiverecek
-				currentPlayer().move(temp); //player önce ilerleyecek sonra o yerin executorını çalıştıracak
+				Square temp = dice.roll(currentPlayer()); 
+				gui.diceRolled(dice);// Dice alacak!!! iki int
+				gui.playerMoved(temp) 
+				//çift atma kontolu
+				if(dice.isEven())
+				currentPlayer().setFinished(false);
+				//
+				currentPlayer().moveTo(board, players, temp; //player önce ilerleyecek sonra o yerin executorını çalıştıracak
 				
 				if(currentPlayer().isFinished()){
-					currentPlayerID = (currentPlayerID+1)%(players.length()-1);
+					currentPlayerID = (currentPlayerID+1)%(players.length-1);
 					if(!(currentPlayer().isBankrupt()))
-					GuiHandler.displayPlayer(currentPlayer()); 
+					gui.displayPlayer(currentPlayer()); 
 				}
 				
 					
 			}
 			else{
-				currentPlayerID = (currentPlayerID+1)%(players.length()-1);
+				currentPlayerID = (currentPlayerID+1)%(players.length-1);
 				if(!(currentPlayer().isBankrupt()))
-					GuiHandler.displayPlayer(currentPlayer());
+					gui.displayPlayer(currentPlayer());
 			}
 		
 	}
@@ -64,19 +78,18 @@ public class Game {
 		return players[currentPlayerID];
 	}
 	
-	private static  beBankrupt(int plID) {
+	private static  void beBankrupt(int plID) {
 		numOfPlayers--;
 		if(numOfPlayers==1)
 			finishGame();
 		players[plID].bankrupt = true;
 		//Batarsa bir oyuncu o oyuncuyu sil(array ve int), mallarını ne yap?, kartlarını ne yap? pending bağlamı?
 		//GUI bağlamında, artık o adam ekranda gözükmesin me?
-		players[plID].releaseAllProperties();
+		players[plID].releaseAllProperty();
 		
 	}
 	private static void finishGame() {
-		// Oyunu bitir
-		// GUI!!!
+		//GUI bitir ve kazananı ilan et
 	}
 
 }
