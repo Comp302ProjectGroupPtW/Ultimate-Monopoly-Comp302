a3040789491362e79a806402dd68e7694ec5e92d
Ata <akesfeden@users.noreply.github.com>
2015-10-18 13:12:06 +0000

    Fatih 15 dk ?nce falan de?i?iklik yapm?? g?r?n?yosun onlar olmayabilir bu halinde.

--- src/Game.java
+++ src/Game.java
@@ -1,6 +1,3 @@
-package src;
-
-
 public class Game {
 
 	public static int numOfPlayers;
@@ -9,6 +6,8 @@ public class Game {
 	public static int currentPlayerID;
 	public static Dice dice;
 	public static GuiHandler gui;
+	
+	private static Game self = new Game();
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
@@ -17,11 +16,11 @@ public class Game {
 		//Game Initializers
 			//GUI will inform how many users there will be (unnecessary for prototype:4)
 		numOfPlayers=4;
-		int startMoney=0;
+		int startMoney=2000;
 		
 			//Board Initializers
 		
-			board = new PrototypeBoard();
+			board = new PrototypeBoard().createBoard();
 			board.pender = null;
 			//Dice initializer
 			
@@ -37,14 +36,15 @@ public class Game {
 			for (int i = 0; i < players.length; i++) {
 				players[i] = new Player(i, names[i], startMoney, board.getSquareById(0));
 			}
-			//bütün playerları idleriyle construct et. for loop
-			currentPlayerID=0; // save load için burası modifiye edilecek
+			//bÃ¼tÃ¼n playerlarÄ± idleriyle construct et. for loop
+			currentPlayerID=0; // save load iÃ§in burasÄ± modifiye edilecek
 			//GUI INIT
+			gui = new GuiHandler(self);
 			gui.init(players, board);
 			gui.displayPlayer(currentPlayer()); //input olarak Player type
 		
 	}
-	private void userRoll() {
+	public void userRoll() {
 		// TODO Auto-generated method stub
 		//Main Loop
 		//GUI nereden updatelenecek???
@@ -54,22 +54,22 @@ public class Game {
 				
 				Square temp = dice.roll(currentPlayer()); 
 				gui.diceRolled(dice);// Dice alacak!!! iki int
-				gui.playerMoved(currentPlayer(),temp); 
+				gui.playerMoved(currentPlayer(), temp);
+				currentPlayer().moveTo(board, players, board.getSquareId(temp));
 				
-				//çift atma kontolu
+				//Ã§ift atma kontolu
 				if(dice.isEven())
 				currentPlayer().setFinished(false);
 				dice = new Dice(board);
 				//
 				
-				//pending için
+				//pending iÃ§in
 				if(board.getPending()){
 					if(currentPlayer()!=board.pender){
 						if(temp instanceof Property){
 							if(((Property) temp).getOwner()==board.pender)
 							{
 								currentPlayer().withdraw(50);
-								board.pending=false;
 							}
 						}
 					}
@@ -77,7 +77,7 @@ public class Game {
 			
 				//
 				
-				currentPlayer().moveTo(board, players, board.getSquareId(temp)); //player önce ilerleyecek sonra o yerin executorını çalıştıracak
+				currentPlayer().moveTo(board, players, board.getSquareId(temp)); //player Ã¶nce ilerleyecek sonra o yerin executorÄ±nÄ± Ã§alÄ±ÅŸtÄ±racak
 				
 				if(currentPlayer().isFinished()){
 					currentPlayerID = (currentPlayerID+1)%(players.length-1);
@@ -105,22 +105,27 @@ public class Game {
 		if(numOfPlayers==1)
 			finishGame();
 		players[plID].bankrupt = true;
-		//Batarsa bir oyuncu o oyuncuyu sil(array ve int), mallarını ne yap?, kartlarını ne yap? pending bağlamı?
-		//GUI bağlamında, artık o adam ekranda gözükmesin me?
+		//Batarsa bir oyuncu o oyuncuyu sil(array ve int), mallarÄ±nÄ± ne yap?, kartlarÄ±nÄ± ne yap? pending baÄŸlamÄ±?
+		//GUI baÄŸlamÄ±nda, artÄ±k o adam ekranda gÃ¶zÃ¼kmesin me?
 		players[plID].releaseAllProperty();
-		if(board.pender==players[plID]){
-			board.pending=false;
-		}
 		
 	}
 	private static void finishGame() {
-		//GUI bitir ve kazananı ilan et
+		//GUI bitir ve kazananÄ± ilan et
 		Player winnerPlayer=null;
 		for (int i = 0; i < players.length; i++) {
 			if(!players[i].isBankrupt())
 				winnerPlayer = players[i];
 		}
 		gui.finish(winnerPlayer);
+	}
+	
+	public static void show(String message){
+		gui.showMessage(message, "Message");
+	}
+	
+	public static boolean ask(String message){
+		return gui.askYesNo(message, "Confirm");
 	}
 
 }
