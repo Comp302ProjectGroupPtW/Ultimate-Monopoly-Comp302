811722cdc44fe0f02abc2a4bcf5abbef8effc0f1
eyirik13 <eyirik13@ku.edu.tr>
2015-10-18 00:09:54 +0000

    getSquares ekledim

--- src/Board.java
+++ src/Board.java
@@ -4,10 +4,13 @@ import java.util.ArrayList;
 public class Board { //superclass
 	private ArrayList<Square> Squares = new ArrayList<Square>(); 
 	private boolean pending = false; //renovation success adlı community chest için
-	public Player pender;
 
 	public void addSquare(Square sq){
 		Squares.add(sq);
+	}
+	
+	public ArrayList<Square> getSquares(){
+		return Squares;
 	}
 
 	public Square getSquareById(int i){
