702fe7c5f4b9f7b3849d9cb5a8ed024c799d0822
ELIF ERBIL <eerbil13@ku.edu.tr>
2015-12-30 12:05:19 +0000

    StockExchange.java edited online with Bitbucket

--- src/domain/StockExchange.java
+++ src/domain/StockExchange.java
@@ -1,6 +1,7 @@
 package domain;
 
 
+
 public class StockExchange extends Square {
 
 	public StockExchange(String name, int id){
@@ -11,8 +12,22 @@ public class StockExchange extends Square {
 
 	@Override
 	public void squareAction(Player currentPlayer, Board board) {
-		// TODO Auto-generated method stub
-
+		for(Player p: Game.getInstance().getPlayers()){
+			for(Company c: Board.getInstance().getCompanyArray()){
+				if(c.getPlayerShareAmount(p)>0){
+					p.deposit(c.getPlayerMoneyReceiveAmount(p));
+				}
+			}
+		}
+		boolean buy = GuiHandler.getInstance().askYesNo("Do you want to buy a share?", "Buy Share");
+		if(buy){
+			Company c = GuiHandler.getInstance().askSelection("Which company share do you want to buy?", "Buy Share", Board.getInstance().getCompanyArray());
+			currentPlayer.buyShare(c);
+		} else{
+			Company c = GuiHandler.getInstance().askSelection("Which company share do you want to buy?", "Auction Share", Board.getInstance().getCompanyArray())
+			AuctionHandler.getInstance().makeAuctionShare(c);
+		}
+		
 	}
 
 }
