d993871484f30fbea58d62cb48a393c240bf1dae
hkamac13 <hkamac13@ku.edu.tr>
2016-01-01 16:03:49 +0000

    EntertainmentRocks.java created online with Bitbucket

---
+++ src/domain/EntertainmentRocks.java
@@ -0,0 +1,26 @@
+package domain;
+
+public class EntertainmentRocks extends ChanceCard {
+
+	EntertainmentRocks() {
+		super("Entertainment Rocks!", "Stakeholders in Motion Pictures \n "
+				+ "and General Radio can immediately \n"
+				+ "collect dividends");
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void cardAction() {
+		// TODO Auto-generated method stub
+		for(Player p: Game.getInstance().getPlayers()){
+			for(Company c: Board.getInstance().getCompanyArray()){
+				if(c.getName().equals("Motion Pictures") || c.getName().equals("General Radio"))
+					if(c.getPlayerShareAmount(p)>0){
+						p.deposit(c.getPlayerMoneyReceiveAmount(p));
+					}
+			}
+		}
+		
+		GuiHandler.getInstance().showMessage(getDescription(), "Card");
+	}
+}
