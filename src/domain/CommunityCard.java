3054aad30dfe16b8f21001f18450fc8bac24e1d3
Elif Erbil <eerbil@users.noreply.github.com>
2016-01-04 21:10:01 +0000

    CommunityCard.java edited online with Bitbucket

--- src/domain/CommunityCard.java
+++ src/domain/CommunityCard.java
@@ -2,8 +2,12 @@ package domain;
 
 
 public abstract class CommunityCard extends Card {
+	
+	public CommunityCard() {
+		// TODO Auto-generated constructor stub
+	}
 
-	public CommunityCard(String name, String description) {
+	CommunityCard(String name, String description) {
 		super(name, description);
 		// TODO Auto-generated constructor stub
 	}
